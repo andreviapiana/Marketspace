bab1312f14dea69bb71916a758c0b57d60efdeb1
andreviapiana <andreviapiana@gmail.com>
2023-07-23 18:54:25 -0300

    ✨feat: Instalação do NativeBase

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -7,18 +7,19 @@ import {
 } from '@expo-google-fonts/karla'
 
 import { SignIn } from '@screens/SignIn'
+import { NativeBaseProvider } from 'native-base'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
 
   return (
-    <>
+    <NativeBaseProvider>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
       {fontsLoaded ? <Text>Hello World</Text> : <SignIn />}
-    </>
+    </NativeBaseProvider>
   )
 }
