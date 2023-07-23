252ec97d487cab55ae1fc10a080dbd3bd25a849b
andreviapiana <andreviapiana@gmail.com>
2023-07-23 18:19:58 -0300

    ✨feat: Fontes Personalizadas do Google Fonts

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,16 @@
-import { StatusBar } from 'react-native'
+import { StatusBar, Text } from 'react-native'
+
+import {
+  useFonts,
+  Karla_400Regular,
+  Karla_700Bold,
+} from '@expo-google-fonts/karla'
 
 import { SignIn } from '@screens/SignIn'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
+
   return (
     <>
       <StatusBar
@@ -10,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <SignIn />
+      {fontsLoaded ? <Text>Hello World</Text> : <SignIn />}
     </>
   )
 }
