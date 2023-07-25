7dcb8b221be8588a747b5ccbc62960f11e2f17f2
andreviapiana <andreviapiana@gmail.com>
2023-07-25 15:08:19 -0300

    ✨feat: Rotas Privadas - Alterando os ícones da Bottom Navigator

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -8,18 +8,33 @@ import { MyAds } from '@screens/MyAds'
 import { NewAndEdit } from '@screens/NewAndEdit'
 import { Product } from '@screens/Product'
 
+import { useTheme } from 'native-base'
+
+import HomeSvg from '@assets/home.svg'
+import MyAdsSvg from '@assets/myads.svg'
+import SignOutSvg from '@assets/signout.svg'
+
 type AppRoutes = {
   home: undefined
   myads: undefined
   newandedit: undefined
   product: undefined
+  signOut: undefined
 }
 
 export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>
 
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>()
 
+function LogoutComponent() {
+  return null
+}
+
 export function AppRoutes() {
+  // Estilo e Tamanho dos Ícones do Menu Inferior //
+  const { sizes, colors } = useTheme()
+  const iconSize = sizes[6]
+
   return (
     <Navigator
       screenOptions={{
@@ -27,13 +42,58 @@ export function AppRoutes() {
         tabBarShowLabel: false,
       }}
     >
-      <Screen name="home" component={Home} />
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
 
-      <Screen name="myads" component={MyAds} />
+      <Screen
+        name="myads"
+        component={MyAds}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <MyAdsSvg
+              fill={color}
+              stroke={color}
+              width={iconSize}
+              height={iconSize}
+            />
+          ),
+        }}
+      />
 
-      <Screen name="newandedit" component={NewAndEdit} />
+      <Screen
+        name="signOut"
+        component={LogoutComponent}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <SignOutSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+        listeners={{
+          tabPress: (e) => {
+            e.preventDefault()
+            /* signOut() */
+          },
+        }}
+      />
 
-      <Screen name="product" component={Product} />
+      <Screen
+        name="newandedit"
+        component={NewAndEdit}
+        options={{ tabBarButton: () => null, tabBarStyle: { display: 'none' } }}
+      />
+
+      <Screen
+        name="product"
+        component={Product}
+        options={{ tabBarButton: () => null, tabBarStyle: { display: 'none' } }}
+      />
     </Navigator>
   )
 }
