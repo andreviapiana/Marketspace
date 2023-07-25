e6bcf01eeeff1816cbab73c63c6f3e03f5a4dd70
andreviapiana <andreviapiana@gmail.com>
2023-07-25 09:33:12 -0300

    ✨feat: Rotas Privadas - Criando Telas das Rotas

---
+++ mobile/src/screens/NewAndEdit/index.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base'
+
+export function NewAndEdit() {
+  return (
+    <Center flex={1}>
+      <Text>NewAndEdit</Text>
+    </Center>
+  )
+}
---
+++ mobile/src/screens/Product/index.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from 'native-base'
+
+export function Product() {
+  return (
+    <Center flex={1}>
+      <Text>Product</Text>
+    </Center>
+  )
+}
