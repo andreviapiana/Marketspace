74cd177f2987988394dc9a721f3cc79222532ee8
andreviapiana <andreviapiana@gmail.com>
2023-07-25 16:01:36 -0300

    ✨feat: SignUp - Validação da Confirmação de Senha

--- mobile/src/screens/SignUp/index.tsx
+++ mobile/src/screens/SignUp/index.tsx
@@ -55,6 +55,10 @@ const signUpSchema = yup.object({
     .string()
     .required('Informe a senha')
     .min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+  password_confirm: yup
+    .string()
+    .required('Confirme a senha.')
+    .oneOf([yup.ref('password')], 'A confirmação da senha não confere'),
 })
 
 export function SignUp() {
@@ -297,6 +301,7 @@ export function SignUp() {
                 value={value}
                 onSubmitEditing={handleSubmit(handleSignUp)}
                 returnKeyType="send"
+                errorMessage={errors.password_confirm?.message}
               />
             )}
           />
