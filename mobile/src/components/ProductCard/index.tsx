f4ba90baae9151d6b6c7ceee42f7f35b0a9fd7ee
andreviapiana <andreviapiana@gmail.com>
2023-07-31 15:04:53 -0300

    ✨feat: MyAds - Criação Completa + Ajuste no ProductCard

--- mobile/src/components/ProductCard/index.tsx
+++ mobile/src/components/ProductCard/index.tsx
@@ -1,29 +1,35 @@
 import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
-import { Heading, Image, Text, VStack, View } from 'native-base'
+import { Box, Heading, Image, Text, VStack, View } from 'native-base'
 
 import LuminariaImg from '@assets/luminaria.png'
 
 import { UserPhoto } from '@components/UserPhoto'
 import { ProductTag } from '@components/ProductTag'
 
-type Props = TouchableOpacityProps & {}
+type Props = TouchableOpacityProps & {
+  isAdDisabled?: boolean
+  hideUserAvatar?: boolean
+}
 
-export function ProductCard({ ...rest }: Props) {
+export function ProductCard({ isAdDisabled, hideUserAvatar, ...rest }: Props) {
   const is_new = true
+
   return (
     <TouchableOpacity {...rest}>
       <VStack mb={6} position={'relative'}>
-        <UserPhoto
-          size={6}
-          source={{ uri: 'https://github.com/andreviapiana.png' }}
-          alt="Imagem do usuário"
-          position={'absolute'}
-          top={1}
-          left={1}
-          zIndex={10}
-          borderColor="gray.100"
-          borderWidth={1}
-        />
+        {!hideUserAvatar && (
+          <UserPhoto
+            size={6}
+            source={{ uri: 'https://github.com/andreviapiana.png' }}
+            alt="Imagem do usuário"
+            position={'absolute'}
+            top={1}
+            left={1}
+            zIndex={10}
+            borderColor="gray.100"
+            borderWidth={1}
+          />
+        )}
         <View position={'absolute'} top={1} right={1} zIndex={10}>
           <ProductTag is_new={is_new} />
         </View>
@@ -36,11 +42,42 @@ export function ProductCard({ ...rest }: Props) {
           resizeMode="center"
         />
 
-        <Text fontSize="sm" color="gray.600" mt={1} numberOfLines={2}>
+        {isAdDisabled && (
+          <Box
+            backgroundColor={'rgba(0,0,0,0.5)'}
+            zIndex={999}
+            w={154}
+            h={100}
+            rounded="md"
+            position={'absolute'}
+          >
+            <Text
+              fontSize={11}
+              color={'gray.100'}
+              position={'absolute'}
+              bottom={1}
+              left={2}
+              fontWeight={'bold'}
+            >
+              ANÚNCIO DESATIVADO
+            </Text>
+          </Box>
+        )}
+
+        <Text
+          fontSize="sm"
+          color={isAdDisabled ? 'gray.400' : 'gray.600'}
+          mt={1}
+          numberOfLines={2}
+        >
           Luminária pendente
         </Text>
 
-        <Heading fontSize="md" color="gray.700" fontFamily={'heading'}>
+        <Heading
+          fontSize="md"
+          color={isAdDisabled ? 'gray.400' : 'gray.700'}
+          fontFamily={'heading'}
+        >
           <Text fontSize={'xs'}>R$&nbsp;</Text>
           59,90
         </Heading>
--- mobile/src/screens/MyAds/index.tsx
+++ mobile/src/screens/MyAds/index.tsx
@@ -1,9 +1,157 @@
-import { Center, Text } from 'native-base'
+import {
+  HStack,
+  Heading,
+  Icon,
+  IconButton,
+  VStack,
+  Text,
+  Select,
+  CheckIcon,
+  FlatList,
+} from 'native-base'
+
+import { Feather } from '@expo/vector-icons'
+
+import { ProductCard } from '@components/ProductCard'
+
+import { useState } from 'react'
+
+import { useNavigation } from '@react-navigation/native'
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+const PRODUCTS = [
+  {
+    id: '1',
+    name: 'Luminária Pendente',
+    description:
+      'Essa é a melhor luminária do mundo. Você não vai se arrepender',
+    is_new: true,
+    price: '59,90',
+    accept_trade: true,
+    user_id: '1',
+    is_active: true,
+    created_at: '2022-11-14T19:31:48.662Z',
+    updated_at: '2022-11-14T19:31:48.662Z',
+  },
+  {
+    id: '2',
+    name: 'Luminária Pendente',
+    description:
+      'Essa é a melhor luminária do mundo. Você não vai se arrepender',
+    is_new: true,
+    price: '29,90',
+    accept_trade: true,
+    user_id: '1',
+    is_active: true,
+    created_at: '2022-11-14T19:31:48.662Z',
+    updated_at: '2022-11-14T19:31:48.662Z',
+  },
+  {
+    id: '3',
+    name: 'Luminária Pendente',
+    description:
+      'Essa é a melhor luminária do mundo. Você não vai se arrepender',
+    is_new: true,
+    price: '59,90',
+    accept_trade: true,
+    user_id: '1',
+    is_active: true,
+    created_at: '2022-11-14T19:31:48.662Z',
+    updated_at: '2022-11-14T19:31:48.662Z',
+  },
+  {
+    id: '4',
+    name: 'Luminária Pendente',
+    description:
+      'Essa é a melhor luminária do mundo. Você não vai se arrepender',
+    is_new: true,
+    price: '59,90',
+    accept_trade: true,
+    user_id: '1',
+    is_active: true,
+    created_at: '2022-11-14T19:31:48.662Z',
+    updated_at: '2022-11-14T19:31:48.662Z',
+  },
+]
 
 export function MyAds() {
+  // State inicial do Filtro dos Meus Anúncios //
+  const [filter, setFilter] = useState('all')
+
+  // Navegando para a Criação de Anúncios //
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  async function handleCreateNewAd() {
+    console.log('BOTÃO DE CRIAÇÃO => CLICOU EM CRIAR NOVO AD')
+    navigation.navigate('newandedit')
+  }
+
   return (
-    <Center flex={1}>
-      <Text>MyAds</Text>
-    </Center>
+    <VStack flex={1} mt={65}>
+      <HStack alignItems="center" justifyContent={'center'} paddingX={6}>
+        <Heading
+          color="gray.700"
+          fontSize="lg"
+          fontFamily={'heading'}
+          position={'relative'}
+        >
+          Meus anúncios
+        </Heading>
+
+        <IconButton
+          rounded="full"
+          justifyContent={'flex-end'}
+          position={'absolute'}
+          right={4}
+          icon={<Icon as={Feather} name="plus" color="gray.700" size="lg" />}
+          onPress={handleCreateNewAd}
+        />
+      </HStack>
+
+      <HStack
+        mt={8}
+        mb={5}
+        paddingX={6}
+        justifyContent={'space-between'}
+        alignItems={'center'}
+      >
+        <Text>9 anúncios</Text>
+
+        <Select
+          selectedValue={filter}
+          height={8}
+          width={120}
+          px={3}
+          py={2}
+          accessibilityLabel="Selecione o tipo do anúncio"
+          defaultValue={'all'}
+          fontSize={'sm'}
+          _selectedItem={{
+            bg: 'blue.200',
+            endIcon: <CheckIcon size="5" />,
+          }}
+          mt={1}
+          onValueChange={(itemValue) => setFilter(itemValue)}
+        >
+          <Select.Item label="Todos" value="all" />
+          <Select.Item label="Ativos" value="active" />
+          <Select.Item label="Inativos" value="inactive" />
+        </Select>
+      </HStack>
+
+      <FlatList
+        data={PRODUCTS}
+        keyExtractor={(item) => item.id}
+        renderItem={() => <ProductCard isAdDisabled hideUserAvatar />}
+        showsVerticalScrollIndicator={false}
+        _contentContainerStyle={{
+          paddingBottom: 4,
+        }}
+        numColumns={2}
+        columnWrapperStyle={{ justifyContent: 'space-between' }}
+        px={6}
+        mt={2}
+      />
+    </VStack>
   )
 }
