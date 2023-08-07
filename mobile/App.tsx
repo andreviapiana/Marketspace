24ea2b368fde0485bd70fddc557c71421efce092
andreviapiana <andreviapiana@gmail.com>
2023-08-07 20:55:44 -0300

    ✨feat: Contextos - Unificando o Create e Provider Context

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -11,7 +11,7 @@ import {
 import { Loading } from '@components/Loading'
 import { Routes } from '@routes/index'
 
-import { AuthContext } from '@contexts/AuthContext'
+import { AuthContextProvider } from '@contexts/AuthContext'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
@@ -23,19 +23,9 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <AuthContext.Provider
-        value={{
-          user: {
-            id: '1',
-            name: 'Rodrigo Gonçalves',
-            email: 'rodrigo@email.com',
-            tel: '54999999999',
-            avatar: 'rodrigo.png',
-          },
-        }}
-      >
+      <AuthContextProvider>
         {fontsLoaded ? <Routes /> : <Loading />}
-      </AuthContext.Provider>
+      </AuthContextProvider>
     </NativeBaseProvider>
   )
 }
