e70db9d2617d2e1c95ad6a8a3cc55760bdef8544
andreviapiana <andreviapiana@gmail.com>
2023-08-08 09:41:08 -0300

    ✨feat: Contextos - Persistindo Usuário Logado(salvando no AsyncStorage)

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -3,6 +3,7 @@ import { ReactNode, createContext, useState } from 'react'
 
 import { UserDTO } from '@dtos/UserDTO'
 import { api } from '@services/api'
+import { storageUserSave } from '@storage/storageUser'
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -26,6 +27,7 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
 
       if (data.user) {
         setUser(data.user)
+        storageUserSave(data.user)
       }
     } catch (error) {
       throw error
