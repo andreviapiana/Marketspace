efa6b9a26dc144638ad9a81ac387d3763f6ddb3e
andreviapiana <andreviapiana@gmail.com>
2023-08-30 14:26:09 -0300

    ✨feat: Refresh Token - Incluindo Gerenciador de Token na API

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -104,6 +104,15 @@ export function AuthContextProvider({ children }: AuthContextProviderProps) {
     loadUserData()
   }, [])
 
+  // useEffect de Interceptação do Token //
+  useEffect(() => {
+    const subscribe = api.registerInterceptTokenManager(signOut)
+
+    return () => {
+      subscribe()
+    }
+  }, [])
+
   return (
     <AuthContext.Provider
       value={{ user, signIn, isLoadingUserStorageData, signOut }}
